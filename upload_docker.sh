bbbc56e559ff5d6ea602fe6aef6cf62ca194ae87
Ragav <kragavendra1100@gmail.com>
2023-03-25 16:50:07 +0530

    step4: upload docker completed and working fine

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=kragavendra1100/predictions-app
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username kragavendra1100
+docker tag predictions-app $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
