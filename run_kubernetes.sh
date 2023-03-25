2970f62acb77032d0c029dba29425c10df5d965c
Ragav <kragavendra1100@gmail.com>
2023-03-25 20:05:54 +0530

    step 6: run kubernetes complete and working fine

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,21 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+USERID=kragavendra1100
+IMAGE=predictions-app
+dockerpath=$USERID/$IMAGE
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run $IMAGE --image=$USERID/$IMAGE --port=80
+# Added logic for wait till the pod is created  
+sleep 180
+kubectl wait --for=condition=ready pods/$IMAGE
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward $IMAGE 8000:80
